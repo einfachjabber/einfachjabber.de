f05c2c218ee135be0dcf58a7bcde2913b93e8a67
Ben Zimmer <benjamin.zimmer@infopark.de>
2010-09-08 13:18:13 +0200

    added MANIFEST.in corrected cherrpy to cherrypy in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'cherrpy',
+        'cherrypy',
         'creoleparser',
         'Flask',
         'Flask-Mail',

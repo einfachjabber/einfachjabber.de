0058e27d4e73f5d3f609e03d003cb070bfe774ce
Ben Zimmer <benjamin.zimmer@infopark.de>
2010-12-15 15:41:05 +0100

    version push to 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='einfachjabber',
-    version='1.0',
+    version='1.5',
     long_description=__doc__,
     packages=['einfachjabber'],
     include_package_data=True,

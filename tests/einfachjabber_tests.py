fd72df15587b4c2c11cc8c8165212cad841719c3
Ben Zimmer <benjamin.zimmer@infopark.de>
2010-12-15 17:58:14 +0100

    started convert testing to use Flask-Testing

--- tests/einfachjabber_tests.py
+++ tests/einfachjabber_tests.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-    test_einfachjabber
-    ~~~~~~~~~~~~~~~
+    einfachjabber.de Tests
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests einfachjabber.de app
 
     :copyright: (c) 2010 by Benjamin Zimmer.
     :license: MIT, see LICENSE for more details.
 """
 
 # -*- coding: utf-8 -*-
+from flaskext.testing import TestCase
+import einfachjabber
 from os import chdir
 from glob import glob
 import re
-import einfachjabber
-import unittest
 import tempfile
 
-class EinfachjabberTestCase(unittest.TestCase):
+class EinfachjabberTestCase(TestCase):
 
     def setUp(self):
         self.app = einfachjabber.app.test_client()

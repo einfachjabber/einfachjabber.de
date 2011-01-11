24fbfd4fbfd7becbcf9ec28f679b74bdbb7b1c66
Ben Zimmer <benjamin.zimmer@infopark.de>
2011-01-11 14:36:03 +0100

    Basic version of stats-app
    Fetches statistics from piwik and displays visitors and number of
    visitors that completed the tutorial.

--- einfachjabber/apps/stats/helpers.py
+++ einfachjabber/apps/stats/helpers.py
@@ -1,14 +1,38 @@
 from piwik import PiwikAPI
 
-def pagesMonthly():
+def pagesMonthly(date):
     piwik = PiwikAPI("http://stats.firefly-it.de/", "anonymous")
+    tutorials = piwik.call(
+                'Actions.getPageUrls',
+                params = {
+                    'idSite': 5,
+                    'period': 'month',
+                    'date': date,
+                    'expanded': 1
+                }
+            )
+    for tutorial in tutorials:
+        if tutorial['label'] == 'tutorial':
+            subtable = tutorial['idsubdatatable']
     pages = piwik.call(
                 'Actions.getPageUrls',
                 params = {
                     'idSite': 5,
                     'period': 'month',
-                    'date': 'today',
-                    'idSubtable': 163
+                    'date': date,
+                    'idSubtable': subtable,
+                    'expanded': 1
                 }
             )
-    return pages
+    print pages
+    data = []
+    for page in pages:
+        if page['label'] != '/index':
+            for subtable in page['subtable']:
+                if subtable['label'] == '/links':
+                    endpagevisits = subtable['nb_visits']
+            data.append({'label': page['label'],
+                        'visits': page['nb_visits'],
+                        'endpagevisits': endpagevisits
+                        })
+    return data
--- einfachjabber/apps/stats/views.py
+++ einfachjabber/apps/stats/views.py
@@ -3,7 +3,7 @@ from flask import render_template
 from einfachjabber.apps.stats import stats
 from einfachjabber.apps.stats.helpers import pagesMonthly
 
-@stats.route('/')
-def stats_index():
-    pages = pagesMonthly()
-    return render_template('stats/index.html', pages=pages)
+@stats.route('/<date>')
+def stats_index(date):
+    data = pagesMonthly(date)
+    return render_template('stats/index.html', data=data)

180874f1262aef02a83b7b99bebf84d8b6f393f7
Ben Zimmer <benjamin.zimmer@infopark.de>
2012-09-03 11:26:31 +0200

    disabled some servers for regristration

--- einfachjabber/forms.py
+++ einfachjabber/forms.py
@@ -15,16 +15,16 @@ from wtforms import Form, TextField, TextAreaField, SelectField,\
 from einfachjabber.utils import sendmail
 
 serverlist = [
-    ('alpha-labs.net', 'alpha-labs.net'),
+    #('alpha-labs.net', 'alpha-labs.net'),
     ('einfachjabber.de', 'einfachjabber.de'),
-    ('jabber.rootbash.com', 'jabber.rootbash.com'),
+    #('jabber.rootbash.com', 'jabber.rootbash.com'),
     ('jabber-server.de', 'jabber-server.de'),
-    ('jabberim.de', 'jabberim.de'),
-    ('jabme.de', 'jabme.de'),
-    ('labnote.org', 'labnote.org'),
-    ('open-host.de', 'open-host.de'),
-    ('prosody.de', 'prosody.de'),
-    ('tekst.me', 'tekst.me')
+    #('jabberim.de', 'jabberim.de'),
+    ('jabme.de', 'jabme.de')
+    #('labnote.org', 'labnote.org'),
+    #('open-host.de', 'open-host.de'),
+    #('prosody.de', 'prosody.de'),
+    #('tekst.me', 'tekst.me')
 ]
 
 def randomserver():

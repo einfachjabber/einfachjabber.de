5c8082f9da58634e62f69cc16a7f788a4cbba607
Ben Zimmer <benjamin.zimmer@infopark.de>
2015-01-19 10:12:47 +0100

    remove wiki link + bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='einfachjabber',
-    version='1.7.4.5',
+    version='1.7.4.6',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
